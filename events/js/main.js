30578a1cae03b4e7bdc66259c23b636385912b91
DeViousJace <jacemattson@gmail.com>
2020-09-08 20:27:33 -0600

    Finished Excercise 03 - Events

---
+++ events/js/main.js
@@ -0,0 +1,12 @@
+let featureLink = document.querySelector('a.feature.link');
+
+
+function featureLinkHandler(event) {
+    let featureImage = document.querySelector('img.feature');
+    featureImage.src = featureLink.href;
+    featureImage.classList.remove('hidden');
+
+    event.preventDefault();
+}
+
+featureLink.addEventListener('click', featureLinkHandler);
